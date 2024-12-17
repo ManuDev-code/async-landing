931dcaa00be0fbc5f30fc5efbe1a2b04f0c512ff
ManuDev-code <manuelamunozlujan0@gmail.com>
2024-12-17 13:33:26 -0500

    Se hizo conexión por medio de API y fetch

---
+++ src/assets/main.js
@@ -0,0 +1,85 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=9';
+
+const content = null || document.getElementById('content');
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    'X-RapidAPI-Key': 'c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256'
+  }
+};
+
+async function fetchData(urlApi) {
+  const response = await fetch(urlApi, options);
+  const data = await response.json();
+  return data;
+}
+
+(async () => {
+  try {
+    const videos = await fetchData(API);
+    let view = `
+    ${videos.items.map(video => `
+      <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+    `).slice(0, 4).join('')}
+    `;
+    content.innerHTML = view;
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+/*const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCiEKDhv4v0YTc-ZtpABkXZA&part=snippet%2Cid&order=date&maxResults=9';
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '50ad08afa4msh57996269fb8b653p1b90c0jsn5c43a16838e6',
+		'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    try{
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+            <div class="group relative">
+          <div
+            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+          </div>
+          <div class="mt-4 flex justify-between">
+            <h3 class="text-sm text-gray-700">
+              <span aria-hidden="true" class="absolute inset-0"></span>
+              ${video.snippet.title}
+            </h3>
+          </div>
+        </div>
+            `).slice(0,4).join('')}
+        `;
+      content.innerHTML = view;
+    } catch (error){
+        console.log(error);
+    }
+})();*/
